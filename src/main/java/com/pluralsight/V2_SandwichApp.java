a67eda174f0c16162245de4c91ea34c07652a1d1
Pav <robsarmiento@icloud.com>
2026-04-13 13:14:38 -0500

    Reworking code

---
+++ src/main/java/com/pluralsight/V2_SandwichApp.java
@@ -0,0 +1,34 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class V2_SandwichApp {
+    public static void main(String[] args) {
+
+
+        Scanner input = new Scanner(System.in);
+
+        double sandwichPrice = 0.00;
+
+        System.out.println("What Size would you like? (1 or 2) + " +
+                "(1) Regular: base price $5.45 + " +
+                "(2): Large: base price $8.95");
+
+
+        if()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
